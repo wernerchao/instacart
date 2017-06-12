6c6c65321b113dca5ca62293514c27bc18e838b4
Werner Chao <oahcrenrew@gmail.com>
2017-06-11 23:23:32 -0400

    try out gbm method

--- spark_model_load.py
+++ spark_model_load.py
@@ -10,14 +10,22 @@ valid_pred = spark.read.csv('./valid_pred_2.csv', header=True)
 bestModel = MatrixFactorizationModel.load(sc, './model_1')
 orders = spark.read.csv('./orders.csv', header=True)
 
+# Recommend n products for all users.
+products_for_users = bestModel.recommendProductsForUsers(3).collect()
+print len(products_for_users)
+
 # Recommend n products for each user loop.
 test = orders.filter(orders['eval_set'] == 'test')
 test_ids = test.select('user_id')
 test_list = test_ids.rdd.map(lambda x: x[0]).collect()
 rec_ = []
-for user_id in test_list:
-    rec_ = bestModel.recommendProducts(int(user_id), 3)
-
+for i, user_id in enumerate(test_list):
+    print 'Round: ', i
+    if i % 100 == 0:
+        pd.DataFrame(rec_).to_csv('rec_{}.csv'.format(i))
+        rec_ = []
+        print 'Output rec_: ', rec_, ' | ', i
+    rec_.append(bestModel.recommendProducts(int(user_id), 8))
 
 
 # Collect product feature matrix
@@ -38,24 +46,21 @@ def func(iterator):
         userArray.append(x[0])
         userFeaturesArray.append(x[1])
         userFeatureMatrix = np.matrix(userFeaturesArray)
-        userRecommendationArray = userFeatureMatrix*(productFeaturesArraybroadcast.value)
+        userRecommendationArray = userFeatureMatrix * (productFeaturesArraybroadcast.value)
         mappedUserRecommendationArray = []
-
         # Extract ratings from the matrix
         i=0
         for i in range(0,len(userArray)):
-            ratingdict = {}
-            j = 0
-
+            ratingdict={}
+            j=0
             for j in range(0,len(productArrayBroadcast.value)):
-                ratingdict[str(productArrayBroadcast.value[j])] = userRecommendationArray.item((i, j))
-                j = j + 1
-
+                ratingdict[str(productArrayBroadcast.value[j])]=userRecommendationArray.item((i,j))
+                j=j+1
             # Take the top 8 recommendations for the user
-            sort_apps = sorted(ratingdict.keys(), key=lambda x: x[1])[:8]
-            sort_apps = '|'.join(sort_apps)
-            mappedUserRecommendationArray.append((userArray[i], sort_apps))
-            i = i + 1
-    return [x for x in mappedUserRecommendationArray]
+            sort_apps=sorted(ratingdict.keys(), key=lambda x: x[1])[:8]
+            sort_apps='|'.join(sort_apps)
+            mappedUserRecommendationArray.append((userArray[i],sort_apps))
+            i=i+1
+        yield [x for x in mappedUserRecommendationArray]
 
-recommendations = model.userFeatures().repartition(2000).mapPartitions(func)
+recommendations = bestModel.userFeatures().repartition(2000).mapPartitions(func)
